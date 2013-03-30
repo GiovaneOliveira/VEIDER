8837a59f81be793f22293e1ed48bcd2be2777cb7
Giovane de Oliveira <vane1313@msn.com>
2013-03-30 01:25:35 -0300

    Adicionado rodapé

--- php/footer.php
+++ php/footer.php
@@ -3,10 +3,27 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <meta http-equiv="Content-Style-Type" content="text/css">
+<style type="text/css">
+	font{
+		color: #F5F5F5;
+		font-family: Gill, Helvetica, sans-serif; 
+		font-size: 15px;
+		font-weight: bold;
+	}
+</style>
 </head>
-<body style=" margin:0;background-color:orange">
-<?
-	echo "Baxxo";
-?>
+<body style=" margin:0;background-color:#333333">
+	<table cellpadding="0" cellspacing="0" style="width:100%; height:80px;">
+		<tr>
+			<td align="center">
+				<img style="height:42px;" src="../image/veider.png"/>
+			</td>
+		</tr>
+		<tr>
+			<td align="center">
+				<font>Veider Sistema de Reservas &copy;</font>
+			</td>
+		</tr>
+	</table>
 </body>
 </html>
